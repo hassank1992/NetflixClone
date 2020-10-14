4a4b99f17ed5e5fc6ddf9296b564cf8604756062
hassanhhan1992@gmail.com <difference@1992>
2020-10-14 08:58:15 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,6 +3,11 @@ window.onload = () => {
 get_Original(); 
 get_Trending();
 get_TopMovies();
+get_ActionMovies();
+get_ComedyMovies();
+get_HorrorMovies();
+get_RomanceMovies();
+get_AnimeMovies();
 };
 function get_Original(){
  var url='https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'; 
@@ -16,6 +21,30 @@ function get_Trending(){
   var url='https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1'; 
  fetch_Movies(url,"#top","backdrop_path");
  }
+ function get_ActionMovies(){
+   var url='https://api.themoviedb.org/3/discover/movie?api_key=19f84e11932abbc79e6d83f82d6d1045&with_genres=28';
+   fetch_Movies(url,"#action","backdrop_path");
+  }
+
+  function get_ComedyMovies(){
+    var url='https://api.themoviedb.org/3/discover/movie?api_key=19f84e11932abbc79e6d83f82d6d1045&with_genres=35';
+    fetch_Movies(url,"#comedy","backdrop_path");
+   }
+
+   function get_HorrorMovies(){
+    var url='https://api.themoviedb.org/3/discover/movie?api_key=19f84e11932abbc79e6d83f82d6d1045&with_genres=27';
+    fetch_Movies(url,"#horror","backdrop_path");
+   }
+
+   function get_RomanceMovies(){
+    var url='https://api.themoviedb.org/3/discover/movie?api_key=19f84e11932abbc79e6d83f82d6d1045&with_genres=10749';
+    fetch_Movies(url,"#romance","backdrop_path");
+   }
+   function get_AnimeMovies(){
+    var url='https://api.themoviedb.org/3/discover/movie?api_key=19f84e11932abbc79e6d83f82d6d1045&with_genres=16';
+    fetch_Movies(url,"#anime","backdrop_path");
+   }
+
 function fetch_Movies(url,element_Selector,poster_path){
   
     fetch(url).then((response)=>{
